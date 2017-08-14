1a283e8ee5f2e6e8b37954470efef6abe7886c90
ramkumar2611v <30981671+ramkumar2611v@users.noreply.github.com>
2017-08-14 14:17:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/one',function(req,res){
+   res.send("2-licks"); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
