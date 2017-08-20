2c27c2fec33eabb2e3290e4251fa15fac1c89c29
ramkumar2611v <30981671+ramkumar2611v@users.noreply.github.com>
2017-08-20 22:51:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,11 @@ element.onclick=function()
   alert("You've Clicked Text.");
   element.innerHTML="RAM KUMAR";
 };
+var img= document.getElementById('img');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+}
+img.onclick=setInterval(moveright,10);
