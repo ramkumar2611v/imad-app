85b61aaededbc53b1ab5c0cd1420093658be5977
ramkumar2611v <30981671+ramkumar2611v@users.noreply.github.com>
2017-08-25 18:50:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,9 +20,9 @@ submit.onclick=function(){
           if(request.status===200){
                var names=request.responseText;
                names=JSON.parse(names);
-                var list="";
+                var list='';
                 for(var i=0;i<names.length;i++){
-                     list+="<li>"+names[i]+"</li>";
+                     list+='<li>'+names[i]+'</li>';
                     }
                 var ul=document.getElementById("namelist");
                 ul.innerHTML=list;
