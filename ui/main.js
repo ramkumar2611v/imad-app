5cf0f21a26958638f3cd77b2fd80e9a2fa5b91d4
ramkumar2611v <30981671+ramkumar2611v@users.noreply.github.com>
2017-08-25 18:34:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,12 +14,23 @@ var name=nameinput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
     
-    var names=["name1","name2","name3"];
-    var list="";
-    for(var i=0;i<names.length;i++){
-        list+="<li>"+names[i]+"</li>";
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.Done){
+          if(request.status===200){
+               var names=["name1","name2","name3"];
+                var list="";
+                for(var i=0;i<names.length;i++){
+                     list+="<li>"+names[i]+"</li>";
+                    }
+                var ul=document.getElementById("namelist");
+                ul.innerHTML=list;
+   
+          }
+      }  
+    };
+    request.open('GET',"http://ramkumar2611v.imad.hasura-app.io/submit-name?name=" + name,true);
+    request.send(null);
     
 };
